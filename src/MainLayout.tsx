c18475420b074c4e553ff1e5cb70ed6696eb466a
Olexandr <4439897@gmail.com>
2025-02-03 07:29:20 +0200

    [react-exam-project]: change fetch

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import Menu from "./components/Menu.tsx";
 import { Outlet } from "react-router-dom";
+import { getUserToken } from "./service/api.service.ts";
 
 const MainLayout = () => {
+  useEffect(() => {
+    const fetchToken = async () => {
+      const user = await getUserToken();
+      if (user) {
+        console.log(user);
+        localStorage.setItem("user", JSON.stringify(user));
+      }
+    };
+    fetchToken();
+  }, []);
+
   return (
     <>
       <Menu />
--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,1 +1,26 @@
-export const baseUrl = "https://dummyjson.com";
+import { IAuthResponse } from "../models/IAuthResponse.ts";
+
+const baseUrl = "https://dummyjson.com";
+
+export const getUserToken = async (): Promise<IAuthResponse | null> => {
+  try {
+    const response = await fetch(`${baseUrl}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: "emilys",
+        password: "emilyspass",
+        expiresInMins: 1, // optional, defaults to 60
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка при получении токена:", error);
+    return null;
+  }
+};
